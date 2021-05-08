a536ffa0771c1252759b74c17caabce03087896c
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2021-05-09 02:43:40 +0530

    Update 146A.cpp

--- A/146A.cpp
+++ A/146A.cpp
@@ -5,25 +5,26 @@ int main(){
 	cin>>n;
 	string s;
 	cin>>s;
+	int sum1=0,sum2=0;
 	bool f=false;
 	for(int i=0;i<n;i++){
-		if(s[i]=='4'||s[i]=='7'){
-			f=true;
-		}
-		else{
-			f=false;
-		}
+		if(s[i]!='4'&&s[i]!='7'){
+			cout<<"NO";
+			return 0;
 	}
-	if(f){
-		if(stoi(s)%2==0){
-			cout<<"YES";
-		}
-		else{
-		cout<<"NO";
+	else{
+		if(i<n/2)
+            sum1+=s[i]-'0';
+        else
+            sum2+=s[i]-'0';
 	}
+}
+	if(sum1==sum2){
+		cout<<"YES";
 	}
 	else{
 		cout<<"NO";
+
 	}
-	
+
 }
